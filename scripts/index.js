c052b7e2fe7e6054badc71b29e920e1cbcae1d65
desimus <tylermills.school2@gmail.com>
2025-05-01 16:45:08 -0700

    update

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,25 @@
 const porfileEditBtn = document.querySelector(".profile__edit-btn");
 const profileNewPostBtn = document.querySelector(".profile__new-post-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
+const editProfileForm = editProfileModal.querySelector(".modal__form");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
+const newPostNameInput = newPostModal.querySelector("#post-caption-input");
+const newPostLinkInput = newPostModal.querySelector("#post-picture-input");
+const newPostCardForm = newPostModal.querySelector(".modal__form");
 
 porfileEditBtn.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionInput.value = profileDescriptionEl.textContent;
   editProfileModal.classList.add("modal_is-opened");
 });
 
@@ -20,3 +34,20 @@ profileNewPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleEditProfileSubmit(evt) {
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionInput.value;
+  evt.preventDefault();
+  editProfileModal.classList.remove("modal_is-opened");
+}
+newPostCardForm.addEventListener("submit", handleEditProfileSubmit);
+
+function handleAddCardSubmit(evt) {
+  console.log(newPostNameInput);
+  console.log(newPostLinkInput);
+  evt.preventDefault();
+  newPostModal.classList.remove("modal_is-opened");
+}
+
+newPostCardForm.addEventListener("submit", handleAddCardSubmit);
